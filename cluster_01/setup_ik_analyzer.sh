ce0476f2f8a782feb2d8044457ccb4021927096f
Tom.Li <open222333@gmail.com>
2022-09-01 19:05:41 +0800

    20220901

--- cluster_01/setup_ik_analyzer.sh
+++ cluster_01/setup_ik_analyzer.sh
@@ -1,8 +1,4 @@
 # 集群
 docker-compose exec es01 elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v${STACK_VERSION}/elasticsearch-analysis-ik-${STACK_VERSION}.zip
-docker-compose exec es02 elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v${STACK_VERSION}/elasticsearch-analysis-ik-${STACK_VERSION}.zip
-docker-compose exec es03 elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v${STACK_VERSION}/elasticsearch-analysis-ik-${STACK_VERSION}.zip
 # 重啟容器
 docker-compose restart es01
-docker-compose restart es02
-docker-compose restart es03
---
+++ single-node-01/setup_ik_docker_compose.sh
@@ -0,0 +1,3 @@
+docker-compose exec elasticsearch elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v${STACK_VERSION}/elasticsearch-analysis-ik-${STACK_VERSION}.zip
+# 重啟容器
+docker-compose restart elasticsearch
