20a60263aa24455cb839441d4e6993d9c0a3cd5f
Tom.Li <open222333@gmail.com>
2022-09-14 15:29:58 +0800

    20220914

---
+++ cluster_01/echo.sh
@@ -0,0 +1,2 @@
+STACK_VERSION=7.13.3
+echo "elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v${STACK_VERSION}/elasticsearch-analysis-ik-${STACK_VERSION}.zip"
