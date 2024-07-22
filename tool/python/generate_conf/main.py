5fffe42e8a4de0342489a9d0a41197ed91fe18e8
Tom.Li <open222333@gmail.com>
2024-07-22 17:38:55 +0800

    修改 執行多個 conf

--- tool/python/generate_conf/main.py
+++ tool/python/generate_conf/main.py
@@ -1,6 +1,13 @@
-from src.mysql_schema import generate_logstash_config, get_table_structure
+from src.mysql_schema import generate_logstash_all_csv_config, get_table_structure, generate_logstash_csv_config, generate_logstash_all_jdbc_config
+from argparse import ArgumentParser
 import json
 import os
+
+
+parser = ArgumentParser()
+parser.add_argument('-m', '--mode', choices=['all_csv', 'csv', 'jdbc'], default='csv')
+parser.add_argument('-e', '--elastichost', default='elasticsearch')
+args = parser.parse_args()
 
 # 獲取當前腳本文件所在的目錄
 script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -12,19 +19,60 @@ config_file = 'config.json'
 with open(config_file, 'r') as file:
     config_data = json.load(file)
 
-all_configs = ""
-for db_config in config_data["databases"]:
-    host = db_config.get("host")
-    port = int(db_config.get("port", 3306))
-    user = db_config.get("user")
-    password = db_config.get("password")
-    database = db_config.get("database")
-    tables = db_config.get("tables")
+if args.mode == "all_csv":
+    all_configs = ""
+    for db_config in config_data["databases"]:
+        host = db_config.get("host")
+        port = int(db_config.get("port", 3306))
+        user = db_config.get("user")
+        password = db_config.get("password")
+        database = db_config.get("database")
+        tables = db_config.get("tables")
 
-    for table_name in tables:
-        structure = get_table_structure(host, user, password, database, table_name, port)
-        logstash_config = generate_logstash_config(structure, table_name)
-        all_configs += logstash_config + "\n"
+        for table_name in tables:
+            structure = get_table_structure(host, user, password, database, table_name, port)
+            logstash_config = generate_logstash_all_csv_config(structure, table_name)
+            all_configs += logstash_config + "\n"
 
-with open('output.conf', 'w') as f:
-    f.write(all_configs)
+    with open('output.conf', 'w') as f:
+        f.write(all_configs)
+elif args.mode == "csv":
+    for db_config in config_data["databases"]:
+        host = db_config.get("host")
+        port = int(db_config.get("port", 3306))
+        user = db_config.get("user")
+        password = db_config.get("password")
+        database = db_config.get("database")
+        tables = db_config.get("tables")
+
+        for table_name in tables:
+            structure = get_table_structure(host, user, password, database, table_name, port)
+            logstash_config = generate_logstash_csv_config(structure, table_name, elastic_host=args.elastichost)
+
+            with open(f'{table_name}.conf', 'w') as f:
+                f.write(logstash_config)
+elif args.mode == "jdbc":
+    all_configs = ""
+    for db_config in config_data["databases"]:
+        host = db_config.get("host")
+        port = int(db_config.get("port", 3306))
+        user = db_config.get("user")
+        password = db_config.get("password")
+        database = db_config.get("database")
+        tables = db_config.get("tables")
+
+        jdbc_url = f"jdbc:mysql://{host}:{port}/{database}"
+
+        for table_name in tables:
+            structure = get_table_structure(host, user, password, database, table_name, port)
+            logstash_config = generate_logstash_all_jdbc_config(
+                structure,
+                jdbc_url,
+                user,
+                password,
+                elastic_host=args.elastichost
+            )
+            all_configs += logstash_config + "\n"
+
+    with open('output.conf', 'w') as f:
+        f.write(all_configs)
--- tool/python/generate_conf/src/mysql_schema.py
+++ tool/python/generate_conf/src/mysql_schema.py
@@ -32,13 +32,14 @@ def get_table_structure(host, user, password, database, table_name, port=3306):
     return table_structure
 
 
-def generate_logstash_config(structure, table_name):
+def generate_logstash_all_csv_config(structure, table_name, elastic_host="localhost", csv_path="/usr/mysql_backup/csv/"):
     """
     根據表結構生成 Logstash 配置
 
     Args:
         structure (list): 表結構的列表，每個元素是一個包含欄位名稱和欄位類型的元組
         table_name (str): 表名稱
+        elastic_host (str): elastic 主機，預設 localhost
 
     Returns:
         str: 生成的 Logstash 配置
@@ -46,7 +47,7 @@ def generate_logstash_config(structure, table_name):
     input_config = f"""
 input {{
   file {{
-    path => "/path/to/csv_files/{table_name}.txt"
+    path => "{csv_path}{table_name}.txt"
     start_position => "beginning"
     sincedb_path => "/dev/null"
     codec => plain {{
@@ -94,16 +95,16 @@ filter {{
         filter_config += f'        "{field_name}" => "{logstash_type}",\n'
 
     filter_config += """
-      }}
-    }}
-  }}
-}}
+      }
+    }
+  }
+}
 """
 
     output_config = f"""
 output {{
   elasticsearch {{
-    hosts => ["http://localhost:9200"]
+    hosts => ["http://{elastic_host}:9200"]
     index => "index_{table_name}"
     document_id => "%{{id}}"
   }}
@@ -115,7 +116,7 @@ output {{
     return logstash_config
 
 
-def generate_logstash_jdbc_config(structures, jdbc_url, jdbc_user, jdbc_password):
+def generate_logstash_all_jdbc_config(structures, jdbc_url, jdbc_user, jdbc_password, elastic_host="localhost", jar_path="/usr/share/logstash/jdbc_drivers/mysql-connector-java-8.0.23.jar"):
     """
     生成 Logstash JDBC 插件配置文件
 
@@ -141,6 +142,7 @@ def generate_logstash_jdbc_config(structures, jdbc_url, jdbc_user, jdbc_password
         jdbc_url (str): JDBC 連接 URL，用於連接 MySQL 資料庫。例如 "jdbc:mysql://host:port/database"。
         jdbc_user (str): JDBC 連接用戶名。
         jdbc_password (str): JDBC 連接密碼。
+        elastic_host (str): elastic 主機，預設 localhost。
 
     Returns:
         str: 生成的 Logstash 配置文件內容。
@@ -151,7 +153,7 @@ input {
     for table_name, structure in structures.items():
         input_config += f"""
   jdbc {{
-    jdbc_driver_library => "/path/to/mysql-connector-java.jar"
+    jdbc_driver_library => "{jar_path}"
     jdbc_driver_class => "com.mysql.jdbc.Driver"
     jdbc_connection_string => "{jdbc_url}"
     jdbc_user => "{jdbc_user}"
@@ -192,24 +194,104 @@ filter {
             filter_config += f'        "{field_name}" => "{logstash_type}"\n'
 
         filter_config += """
-      }}
-    }}
-  }}
+      }
+    }
+  }
 """
     filter_config += """
 }
 """
 
-    output_config = """
-output {
-  elasticsearch {
-    hosts => ["http://localhost:9200"]
-    index => "index_%{[@metadata][table]}"
-    document_id => "%{id}"
+    output_config = f"""
+output {{
+  elasticsearch {{
+    hosts => ["http://{elastic_host}:9200"]
+    index => "index_%{{[@metadata][table]}}"
+    document_id => "%{{id}}"
+  }}
+  stdout {{ codec => rubydebug }}
+}}
+"""
+
+    logstash_config = input_config + filter_config + output_config
+    return logstash_config
+
+
+def generate_logstash_csv_config(structure, table_name, elastic_host="localhost", csv_path="/usr/mysql_backup/csv/"):
+    """
+    根據表結構生成 Logstash 配置
+
+    Args:
+        structure (list): 表結構的列表，每個元素是一個包含欄位名稱和欄位類型的元組
+        table_name (str): 表名稱
+        elastic_host (str): elastic 主機，預設 localhost
+
+    Returns:
+        str: 生成的 Logstash 配置
+    """
+    # Input 部分
+    input_config = f"""
+input {{
+  file {{
+    path => "{csv_path}{table_name}.txt"
+    start_position => "beginning"
+    sincedb_path => "/dev/null"
+    codec => plain {{
+      charset => "UTF-8"
+    }}
+  }}
+}}
+"""
+
+    # Filter 部分
+    filter_config = f"""
+filter {{
+  csv {{
+    separator => ","
+    columns => [{', '.join([f'"{col[0]}"' for col in structure])}]
+    quote_char => '"'
+  }}
+
+  mutate {{
+    convert => {{
+"""
+
+    # 添加 convert 配置
+    for column in structure:
+        field_name = column[0]
+        field_type = column[1]
+
+        if "int" in field_type:
+            logstash_type = "integer"
+        elif "char" in field_type or "text" in field_type:
+            logstash_type = "string"
+        elif "datetime" in field_type or "timestamp" in field_type:
+            logstash_type = "date"
+        elif "float" in field_type or "double" in field_type or "decimal" in field_type:
+            logstash_type = "float"
+        else:
+            logstash_type = "string"
+
+        filter_config += f'      "{field_name}" => "{logstash_type}",\n'
+
+    filter_config += """
+    }
   }
-  stdout { codec => rubydebug }
 }
 """
 
+    # Output 部分
+    output_config = f"""
+output {{
+  elasticsearch {{
+    hosts => ["http://{elastic_host}:9200"]
+    index => "{table_name}"
+    document_id => "%{{{table_name}_id}}"
+  }}
+  stdout {{ codec => rubydebug }}
+}}
+"""
+
+    # 合併所有部分並返回
     logstash_config = input_config + filter_config + output_config
     return logstash_config
