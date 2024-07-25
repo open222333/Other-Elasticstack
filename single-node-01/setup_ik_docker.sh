8d43bc68f90b3e10bd48141e6680aa57fd5c7cf1
Tom.Li <open222333@gmail.com>
2024-07-25 14:32:42 +0800

    20240725

--- single-node-01/setup_ik_docker.sh
+++ single-node-01/setup_ik_docker.sh
@@ -1,6 +1,35 @@
-# 容器es
-docker exec -it es /bin/bash
-# 使用bin 設計的下載elasticsearch-plugin install 安全名前
-bin/elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v${STACK_VERSION}/elasticsearch-analysis-ik-${STACK_VERSION}.zip
-# 重啟容器
+#!/bin/bash
+
+# 讀取 .env 文件
+source "$(dirname -- "$0")/.env"
+
+# 檢查 STACK_VERSION 是否設置
+if [ -z "$STACK_VERSION" ]; then
+  echo "STACK_VERSION 未設置在 .env 文件中。"
+  exit 1
+fi
+
+# 進入 Elasticsearch 容器並安裝插件
+docker exec -it es /bin/bash -c "
+  # 安裝 elasticsearch-analysis-ik 插件
+  bin/elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v${STACK_VERSION}/elasticsearch-analysis-ik-${STACK_VERSION}.zip
+
+  # 檢查插件是否安裝成功
+  if [ $? -eq 0 ]; then
+    echo 'Plugin installed successfully.'
+  else
+    echo 'Plugin installation failed.'
+    exit 1
+  fi
+"
+
+# 重啟 Elasticsearch 容器
 docker restart es
+
+# 檢查容器是否成功重啟
+if [ $? -eq 0 ]; then
+  echo 'Elasticsearch container restarted successfully.'
+else
+  echo 'Failed to restart Elasticsearch container.'
+  exit 1
+fi
--- single-node-01/setup_ik_docker_compose.sh
+++ single-node-01/setup_ik_docker_compose.sh
@@ -1,4 +1,32 @@
-# 集群
+#!/bin/bash
+
+# 讀取 .env 文件
+source "$(dirname -- "$0")/.env"
+
+# 檢查 STACK_VERSION 是否設置
+if [ -z "$STACK_VERSION" ]; then
+  echo "STACK_VERSION 未設置在 .env 文件中。"
+  exit 1
+fi
+
+# 安裝插件
 docker-compose exec elasticsearch elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v${STACK_VERSION}/elasticsearch-analysis-ik-${STACK_VERSION}.zip
-# 重啟容器
+
+# 檢查插件是否安裝成功
+if [ $? -eq 0 ]; then
+  echo 'Plugin installed successfully.'
+else
+  echo 'Plugin installation failed.'
+  exit 1
+fi
+
+# 重啟 Elasticsearch 容器
 docker-compose restart elasticsearch
+
+# 檢查容器是否成功重啟
+if [ $? -eq 0 ]; then
+  echo 'Elasticsearch container restarted successfully.'
+else
+  echo 'Failed to restart Elasticsearch container.'
+  exit 1
+fi
