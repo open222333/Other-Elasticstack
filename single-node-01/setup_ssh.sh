b4dc0eff9eff98160438c1a6a25b5a387e4b9d6a
Tom.Li <open222333@gmail.com>
2024-07-26 14:10:29 +0800

    修改 腳本

--- single-node-01/setup_ssh.sh
+++ single-node-01/setup_ssh.sh
@@ -2,18 +2,19 @@
 
 # 檢查是否設置了 host_name 和 host_ip 變數
 if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Usage: $0 <host_name> <host_ip>"
+  echo "Usage: $0 <host_name> <host_ip> [user]"
   exit 1
 fi
 
 host_name=$1
 host_ip=$2
+user=${3:-root}  # 默認用戶為 root，若指定了則使用指定的用戶
 
 # 生成 SSH 密鑰對
 ssh-keygen -f ~/.ssh/$host_name
 
 # 複製公鑰到遠程主機
-ssh-copy-id -i ~/.ssh/$host_name.pub root@$host_ip
+ssh-copy-id -i ~/.ssh/$host_name.pub $user@$host_ip
 
 # 檢查 ssh-copy-id 是否成功
 if [ $? -eq 0 ]; then
@@ -24,7 +25,7 @@ else
 fi
 
 # 將新配置追加到 ~/.ssh/config 文件末尾
-echo -e "\nHost $host_name\n    User root\n    HostName $host_ip\n    ServerAliveInterval 60\n    IdentityFile ~/.ssh/$host_name" >> ~/.ssh/config
+echo -e "\nHost $host_name\n    User $user\n    HostName $host_ip\n    ServerAliveInterval 60\n    IdentityFile ~/.ssh/$host_name" >> ~/.ssh/config
 
 # 檢查配置是否成功添加
 if [ $? -eq 0 ]; then
