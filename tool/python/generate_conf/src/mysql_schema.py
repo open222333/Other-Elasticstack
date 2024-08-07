56c2a99b6591fb56ed270f48a90984d0f8a0e863
Tom.Li <open222333@gmail.com>
2024-08-07 14:45:44 +0800

    修改 生成jdbc conf

--- tool/python/generate_conf/src/mysql_schema.py
+++ tool/python/generate_conf/src/mysql_schema.py
@@ -121,7 +121,7 @@ output {{
     return logstash_config
 
 
-def generate_logstash_all_jdbc_config(structures, jdbc_url, jdbc_user, jdbc_password, elastic_host="localhost", jar_path="/usr/share/logstash/jdbc_drivers", jar_name="mysql-connector-java-8.0.23.jar",index_name="\"index_%{[@metadata][table]}\""):
+def generate_logstash_all_jdbc_config(structures, jdbc_url, jdbc_user, jdbc_password, elastic_host="localhost", index_name="\"index_%{[@metadata][table]}\"", **kwargs):
     """
     生成 Logstash JDBC 插件配置文件
 
@@ -149,16 +149,44 @@ def generate_logstash_all_jdbc_config(structures, jdbc_url, jdbc_user, jdbc_pass
         jdbc_password (str): JDBC 連接密碼。
         elastic_host (str): elastic 主機，預設 localhost。
 
+        jar_path (str): jar 放置資料夾, 預設 /usr/share/logstash/jdbc_drivers。
+        jar_version (str): jar 版本, 預設 8.0.23。
+        jar_name (str)L jar 名稱, 預設 mysql-connector-java-<jar_version>.jar
+        jdbc_driver_class (str): jdbc_driver_class 參數, 預設 com.mysql.cj.jdbc.Driver
+        jdbc_paging_enabled => true
+        jdbc_page_size (int): jdbc_page_size 參數, 每次查詢指定數量數據, 預設 關閉
+
     Returns:
         str: 生成的 Logstash 配置文件內容。
     """
     # jdbc_driver_class = "com.mysql.jdbc.Driver"
-    jdbc_driver_class = "com.mysql.cj.jdbc.Driver"
+    jar_version = kwargs.get('jar_version', '8.0.23')
+    jar_name = kwargs.get('jar_name', f'mysql-connector-java-{jar_version}.jar')
+    jar_path = kwargs.get('jar_path', '/usr/share/logstash/jdbc_drivers')
+    jdbc_driver_class = kwargs.get('jdbc_driver_class', "com.mysql.cj.jdbc.Driver")
+    jdbc_page_size = kwargs.get('jdbc_page_size', None)
+
 
 
     input_config = "input {\n"
     for table_name, structure in structures.items():
-        input_config += f"""
+        if jdbc_page_size != None:
+            jdbc_page_size = int(jdbc_page_size)
+            input_config += f"""
+  jdbc {{
+    jdbc_driver_library => "{jar_path}/{jar_name}"
+    jdbc_driver_class => "{jdbc_driver_class}"
+    jdbc_connection_string => "{jdbc_url}"
+    jdbc_user => "{jdbc_user}"
+    jdbc_password => "{jdbc_password}"
+    statement => "SELECT * FROM {table_name}"
+    add_field => {{ "[@metadata][table]" => "{table_name}" }}
+    jdbc_paging_enabled => true
+    jdbc_page_size => {jdbc_page_size}
+  }}
+"""
+        else:
+            input_config += f"""
   jdbc {{
     jdbc_driver_library => "{jar_path}/{jar_name}"
     jdbc_driver_class => "{jdbc_driver_class}"
